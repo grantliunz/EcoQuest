df99e1f2d646f9780f623221d02a899bd32af933
Jordan Wen Jie See <100274076+wsee046@users.noreply.github.com>
2023-07-09 00:17:15 +1200

    enabled location updates

--- app/src/main/java/com/example/devs_hackathon_2023/Map.java
+++ app/src/main/java/com/example/devs_hackathon_2023/Map.java
@@ -7,6 +7,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +23,12 @@ import androidx.fragment.app.Fragment;
 
 import com.example.devs_hackathon_2023.databinding.FragmentMapBinding;
 import com.example.devs_hackathon_2023.utils.PermissionUtils;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.MapStyleOptions;
@@ -37,12 +44,26 @@ public class Map extends Fragment implements OnMapReadyCallback,
     public class Coordinates {
         public double latitude;
         public double longitude;
+
+        Coordinates(double latitude, double longitude){
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
     }
 
     private FragmentMapBinding binding;
     private GoogleMap map;
     private boolean permissionDenied = false;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+    private Location currentLocation;
+    private Coordinates targetLocation;
+
+    public void setTargetLocation(double latitude, double longitude){
+        this.targetLocation = new Coordinates(latitude, longitude);
+    }
 
     @Nullable
     @Override
@@ -55,8 +76,42 @@ public class Map extends Fragment implements OnMapReadyCallback,
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        locationCallback = new LocationCallback() {
+            // callback whenever location updates
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    if (location != null) {
+                        currentLocation = location;
+//                        Log.d("TAG", "loc: lat " + currentLocation.getLatitude() + ", long " + currentLocation.getLongitude());
+                        if(targetLocation != null){
+                            // check if current location is close to target location
+//                            Log.d("TAG", String.valueOf(areTwoCoordinatesClose(new Coordinates(currentLocation.getLatitude(), currentLocation.getLongitude()), targetLocation)));
+                        }
+                    }
+                }
+            }
+        };
+
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.maps);
         mapFragment.getMapAsync(this);
+    }
+
+    private void startLocationUpdates() {
+        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY,5000).setMinUpdateIntervalMillis(2000).setMaxUpdateDelayMillis(8000).build();
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
     }
 
     @Override
@@ -72,6 +127,7 @@ public class Map extends Fragment implements OnMapReadyCallback,
         map.setOnMyLocationClickListener(this);
         enableMyLocation();
         onMyLocationButtonClick();
+        startLocationUpdates();
 
         // Move the camera to the user's location
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
@@ -120,19 +176,11 @@ public class Map extends Fragment implements OnMapReadyCallback,
             }
         }
 
-        Coordinates currentLocation = getCurrentLocation();
-        Log.d("TAG", "loc - lat: " + currentLocation.latitude + ", long: " + currentLocation.longitude);
-        Coordinates targetLocation = new Coordinates();
-        targetLocation.latitude = -36.8509;
-        targetLocation.longitude = 174.7719;
-//        Log.d("TAG", String.valueOf(areTwoCoordinatesClose(currentLocation, targetLocation)));
         return true;
     }
 
     @Override
     public void onMyLocationClick(@NonNull Location location) {
-        Log.d("TAG", "onMyLocationClick: " + location.getLatitude() + " " + location.getLongitude());
-
         Toast.makeText(requireContext(), "Current location:\n" + location, Toast.LENGTH_LONG).show();
     }
 
@@ -158,29 +206,16 @@ public class Map extends Fragment implements OnMapReadyCallback,
             showMissingPermissionError();
             permissionDenied = false;
         }
+        startLocationUpdates();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopLocationUpdates();
     }
 
     private void showMissingPermissionError() {
         PermissionUtils.PermissionDeniedDialog.newInstance(true).show(requireFragmentManager(), "dialog");
-    }
-
-    public Coordinates getCurrentLocation(){
-        if (map != null) {
-                    LocationManager locationManager = (LocationManager) requireContext().getSystemService(Context.LOCATION_SERVICE);
-                    Criteria criteria = new Criteria();
-                    if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                            && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        return null;
-                    }
-                    Location lastKnownLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-                    if (lastKnownLocation != null) {
-                        Coordinates coordinates = new Coordinates();
-                        coordinates.latitude = lastKnownLocation.getLatitude();
-                        coordinates.longitude = lastKnownLocation.getLongitude();
-                        return coordinates;
-                    }
-                }
-        return null;
     }
 
     public boolean areTwoCoordinatesClose(Coordinates first, Coordinates second){
@@ -188,7 +223,7 @@ public class Map extends Fragment implements OnMapReadyCallback,
             return false;
         }
         double distance = Math.sqrt(Math.pow(first.latitude - second.latitude, 2) + Math.pow(first.longitude - second.longitude, 2));
-        return distance < 0.0001;
+        return distance < 0.0003;
     }
 }
 
