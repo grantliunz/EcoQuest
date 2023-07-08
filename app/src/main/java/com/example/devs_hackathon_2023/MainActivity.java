d0cd2f91803be4334e9132ffe426e21f3b7b1c46
Dhruv Joshi <djos192@aucklanduni.ac.nz>
2023-07-08 16:52:53 +1200

    Set up Bottom Nav bar

--- app/src/main/java/com/example/devs_hackathon_2023/MainActivity.java
+++ app/src/main/java/com/example/devs_hackathon_2023/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.devs_hackathon_2023;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.view.View.OnClickListener;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.example.devs_hackathon_2023.activities.ShopActivity;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -27,13 +34,41 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
+    private Map map = new Map();
+    private Quests quests = new Quests();
+    private Social social = new Social();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        BottomNavigationView navView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, map).commit();
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.task_button) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, quests).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.map_button) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, map).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.social_button) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, social).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void replaceFragment(int layoutResId) {
+        View view = getLayoutInflater().inflate(layoutResId, null);
+        FrameLayout container = findViewById(R.id.fragment_container);
+        container.removeAllViews();
+        container.addView(view);
         setupShopButton();
 
     }
