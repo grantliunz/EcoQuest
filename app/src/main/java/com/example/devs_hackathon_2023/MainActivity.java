3b57cda43b38484bbe520a68a3a0f7b18f837c45
Aaron Rodrigues <ardo715@aucklanduni.ac.nz>
2023-07-08 17:07:03 +1200

    Animation

--- app/src/main/java/com/example/devs_hackathon_2023/MainActivity.java
+++ app/src/main/java/com/example/devs_hackathon_2023/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.devs_hackathon_2023;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.view.Menu;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -13,6 +18,8 @@ import com.example.devs_hackathon_2023.activities.ShopActivity;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -26,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    protected View circleView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,9 +66,56 @@ public class MainActivity extends AppCompatActivity {
         clickableImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ShopActivity.class);
-                startActivity(intent);
+                // Handle the click event
+                animateAndSwitch(v);
             }
         });
+    }
+    private void animateAndSwitch(View view) {
+        // Get the click position
+        float startX = 1285;
+        float startY = 330;
+        System.out.println("startX: " + startX);
+        System.out.println("startY: " + startY);
+
+
+        circleView = new View(MainActivity.this);
+
+        // Create a circular view to animate
+        circleView.setLayoutParams(new ViewGroup.LayoutParams(3000, 3000));
+        circleView.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.circle_shape));
+        circleView.setX(0);
+        circleView.setY(view.getHeight());
+        ((ViewGroup) getWindow().getDecorView()).addView(circleView);
+
+        // Define the animation
+        float endRadius = 600;
+        Animator animator = ViewAnimationUtils.createCircularReveal(circleView, (int) startX, (int) startY, 30, 4000);
+        animator.setDuration(800);
+
+        // Set an animation listener
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+//                super.onAnimationEnd(animation);
+                // Remove the circular view
+
+                // Start the pop-up animation
+                startPopUpAnimation();
+            }
+        });
+
+        // Start the animation
+        animator.start();
+
+    }
+
+    private void startPopUpAnimation() {
+        Intent intent = new Intent(MainActivity.this, ShopActivity.class);
+        System.out.println("Starting ShopActivity");
+        // Start the pop-up animation
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeCustomAnimation(MainActivity.this, R.anim.blow_up, R.anim.blow_up);
+        startActivity(intent, options.toBundle());
+
     }
 }
